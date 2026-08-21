d1902ccbdcebe593d104e25a28e2d851050fbe41
SSGOG <gadekarshreyans0209@gmail.com>
2026-08-21 23:27:58 +0530

    Remove Dockerfile, fix port and add onnx dependency

--- app.py
+++ app.py
@@ -55,5 +55,5 @@ with gr.Blocks(title="Underwater Mine Detection") as demo:
     gr.Markdown("Developed by **Devansh, Shreyans**")
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 7860))
+    port = int(os.environ.get('PORT', 10000))
     demo.launch(server_name="0.0.0.0", server_port=port, ssr_mode=False)
